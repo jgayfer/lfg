5b349638cdf5875007bd7791bf61b014802ede18
James Gayfer <gayfer.james@gmail.com>
2022-07-08 09:55:28 -0700

    Remove owner from Integration
    I don't think we're quite at the point yet where this is needed.

--- core/src/model/integration.rs
+++ core/src/model/integration.rs
@@ -1,9 +1,6 @@
-use crate::model::User;
-
 #[derive(Clone, Debug)]
 pub struct Integration {
     pub id: i32,
-    pub owner: User,
     pub webhook_url: String,
     pub success_url: String,
 }
--- support/src/fixtures/test_integration.rs
+++ support/src/fixtures/test_integration.rs
@@ -1,7 +1,5 @@
 use lfg_core::model::Integration;
 
-use super::TestUser;
-
 pub struct TestIntegration {
     integration: Integration,
 }
@@ -11,7 +9,6 @@ impl TestIntegration {
         Self {
             integration: Integration {
                 id: 1,
-                owner: TestUser::new().build(),
                 webhook_url: "https://test-webhook.com".into(),
                 success_url: "https://test-success.com".into(),
             },
