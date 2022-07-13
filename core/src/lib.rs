8120520d2d57a12d9447ecd9a24900438fd1e591
James Gayfer <gayfer.james@gmail.com>
2022-07-12 21:07:11 -0700

    Remove initial models
    These were mostly just to get the brain juices flowing, but we don't
    need them anymore.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -9,23 +9,3 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
-
-struct User {
-    pub id: i32,
-}
-
-struct Authorization {
-    pub id: i32,
-    pub user: User,
-    pub integration: Integration,
-}
-
-struct Integration {
-    pub id: i32,
-}
-
-struct Group {
-    pub id: i32,
-    pub owner: User,
-    pub integration: Integration,
-}
